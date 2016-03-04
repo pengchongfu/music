e89c0ed76d0f71db521a989086444dfd281b6644
pengchongfu <pengchongfu@126.com>
2016-03-04 14:32:05 +0800

    修改ui，基本完成

--- public/javascripts/music.js
+++ public/javascripts/music.js
@@ -1,13 +1,15 @@
 var playList=[];
-var playButton=document.getElementById("playButton");
-var pauseButton=document.getElementById("pauseButton");
-var switchButton=document.getElementById("switchButton");
-var nextButton=document.getElementById("nextButton");
-var previousButton=document.getElementById("previousButton");
+var musicIcon=$("#musicIcon");
 var playState=document.getElementById("playState");
-var listButton=document.getElementById("listButton");
+var leftTime=document.getElementById("leftTime");
 var process=$("#process");
 var bar=$('#bar');
+var previousButton=document.getElementById("previousButton");
+var switchButton=document.getElementById("switchButton");
+var playButton=document.getElementById("playButton");
+var pauseButton=document.getElementById("pauseButton");
+var nextButton=document.getElementById("nextButton");
+var listButton=document.getElementById("listButton");
 var list=$("#list");
 
 //获取音乐列表信息
@@ -30,7 +32,6 @@ var list=$("#list");
 })();
 playList.map(function(item){
     list.append("<li>"+item+"</li>");
-    return;
 });
 
 var audio=document.createElement("audio");
@@ -38,17 +39,34 @@ audio.index=0;
 audio.src='music/'+playList[audio.index];
 updateState();
 
-
+process.click(changeProcess);
+previousButton.addEventListener('click',previousSong);
 switchButton.addEventListener("click",playOrPause);
 nextButton.addEventListener("click",nextSong);
-previousButton.addEventListener('click',previousSong);
 listButton.addEventListener("click",function(){
     list.css("display")==="block"?list.css("display","none"):list.css("display","block");
 });
+$("ul li").click(function(){
+    if(audio.index===$(this).index()){
+        audio.play();
+        updateState();
+        return;
+    }
+    audio.index=$(this).index();
+    switchSong();
+    updateState();
+});
 
-function switchSong(){
-    audio.src='music/'+playList[audio.index];
-    audio.play();
+function changeProcess(e){
+    var persent=(e.pageX-process.offset().left)/process.width();
+    audio.currentTime=audio.duration*persent;
+    bar.css("width",persent*100+"%");
+}
+
+function previousSong(){
+    audio.index-1<0?audio.index=playList.length-1:audio.index-=1;
+    switchSong();
+    updateState();
 }
 
 function playOrPause(){
@@ -67,25 +85,30 @@ function nextSong(){
     updateState();
 }
 
-function previousSong(){
-    audio.index-1<0?audio.index=playList.length-1:audio.index-=1;
-    switchSong();
-    updateState();
+function switchSong(){
+    audio.src='music/'+playList[audio.index];
+    audio.play();
 }
 
 function updateState(){
+    var index=audio.index;
+    var arr=$("ul li");
+    arr.css("color","#000");
+    $(arr[index]).css("color","#eee");
     if(audio.paused){
-        playButton.style.display="inline";
+        playButton.style.display="inline-block";
         pauseButton.style.display="none";
         playState.innerHTML=playList[audio.index];
+        musicIcon.css("animation-play-state","paused");
     }
     else{
         playButton.style.display="none";
-        pauseButton.style.display="inline";
+        pauseButton.style.display="inline-block";
         playState.innerHTML=playList[audio.index];
+        musicIcon.css("animation-play-state","running");
     }
-
 }
+
 
 audio.addEventListener("ended",function(){
     nextSong();
@@ -93,21 +116,17 @@ audio.addEventListener("ended",function(){
 
 audio.addEventListener("timeupdate",function(){
     var persent=audio.currentTime/audio.duration;
+    var left=audio.duration-audio.currentTime;
+    var min=parseInt(left/60);
+    var sec=parseInt(left%60);
+    var str="-"+min+":"+sec;
+    if(sec<10)str="-"+min+":0"+sec;
+    if(isNaN(sec))str="-0:00";
     bar.css("width",persent*100+"%");
+    leftTime.innerHTML=str;
 });
 
-
-process.click(changeProcess);
-
-function changeProcess(e){
-    var persent=(e.pageX-process.offset().left)/process.width();
-    audio.currentTime=audio.duration*persent;
-    bar.css("width",persent*100+"%");
+function formatTime(second) {
+    return [parseInt(second / 60 / 60), second / 60 % 60, second % 60].join(":")
+        .replace(/\b(\d)\b/g, "0$1");
 }
-
-
-$("ul li").click(function(){
-    audio.index=$(this).index();
-    switchSong();
-    updateState();
-});
